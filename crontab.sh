b11e14da04c8641f790e199950b803d5d648577e
Michael2015 <13794888292@163.com>
2018-09-21 09:44:16 +0800

    window

--- crontab.sh
+++ crontab.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /usr/share/nginx/html/www.haixialiangan.wang/haixialiangan/
-/usr/bin/php artisan video
+cd /usr/share/nginx/html/test.haixialiangan.wang/haixialiangan/
+/usr/bin/php  ./artisan video
